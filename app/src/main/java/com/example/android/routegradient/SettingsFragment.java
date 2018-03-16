e4fe43e69398da26bb325588261d96731a7edd2c
McKenzie <mac.gray.311@gmail.com>
2018-03-15 18:32:57 -0700

    added activity and fragment for user prefs

---
+++ app/src/main/java/com/example/android/routegradient/SettingsFragment.java
@@ -0,0 +1,22 @@
+package com.example.android.routegradient;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+/**
+ * Created by McKenzie on 3/15/2018.
+ */
+
+public class SettingsFragment extends PreferenceFragmentCompat
+        implements SharedPreferences.OnSharedPreferenceChangeListener{
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+
+    }
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+
+    }
+}
