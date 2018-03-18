28e525e1d0f8e0e178c102e94566fa8e05b90610
Unknown <bensocar@oregonstate.edu>
2018-03-18 00:25:30 -0700

    Initial commit

---
+++ app/src/main/java/com/example/android/routegradient/NetworkUtils.java
@@ -0,0 +1,28 @@
+package com.example.android.routegradient;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+/**
+ * Created by cbenson on 3/17/18.
+ */
+
+public class NetworkUtils {
+    private static final OkHttpClient mHTTPClient = new OkHttpClient();
+
+    public static String doHTTPGet(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        Response response = mHTTPClient.newCall(request).execute();
+
+        try {
+            return response.body().string();
+        } finally {
+            response.close();
+        }
+    }
+}
