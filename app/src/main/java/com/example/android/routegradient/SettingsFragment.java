478d3ff4b7380de27567771ecad21e37bfbecf0e
McKenzie <mac.gray.311@gmail.com>
2018-03-22 11:24:55 -0700

    removed samples preference

--- app/src/main/java/com/example/android/routegradient/SettingsFragment.java
+++ app/src/main/java/com/example/android/routegradient/SettingsFragment.java
@@ -9,37 +9,10 @@ import android.support.v7.preference.PreferenceFragmentCompat;
  * Created by McKenzie on 3/15/2018.
  */
 
-public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener{
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EditTextPreference samplesPref = (EditTextPreference) findPreference(getString(R.string.pref_samples_key));
-        samplesPref.setSummary(samplesPref.getText());
-    }
+public class SettingsFragment extends PreferenceFragmentCompat{
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.prefs);
-    }
-
-    @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        if (key.equals(getString(R.string.pref_samples_key))) {
-            EditTextPreference samplesPref = (EditTextPreference) findPreference(key);
-            samplesPref.setSummary(samplesPref.getText());
-        }
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
     }
 }
